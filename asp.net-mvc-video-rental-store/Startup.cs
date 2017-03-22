8465afe186b8efa5a2271378fc430843c22de71f
Armin <armin.ag@outlook.com>
2017-03-22 18:02:34 +0100

    Add project files.

---
+++ asp.net-mvc-video-rental-store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp.net_mvc_video_rental_store.Startup))]
+namespace asp.net_mvc_video_rental_store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
