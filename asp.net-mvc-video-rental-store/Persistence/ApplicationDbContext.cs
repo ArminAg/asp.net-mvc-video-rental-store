3152b64abb79929efe8e817f69c887ad6e72c6d6
Armin <armin.ag@outlook.com>
2017-03-22 20:55:30 +0100

    Enable Migrations and Add Initial Model

--- asp.net-mvc-video-rental-store/Persistence/ApplicationDbContext.cs
+++ asp.net-mvc-video-rental-store/Persistence/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
-﻿using asp.net_mvc_video_rental_store.Models;
+﻿using asp.net_mvc_video_rental_store.Core.Models;
+using asp.net_mvc_video_rental_store.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace asp.net_mvc_video_rental_store.Persistence
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
