874e1b9cadf459f166895c08ec1a0108d5a419a2
Armin <armin.ag@outlook.com>
2017-03-22 18:54:47 +0100

    Add Bootstrap Theme

--- asp.net-mvc-video-rental-store/App_Start/BundleConfig.cs
+++ asp.net-mvc-video-rental-store/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace asp.net_mvc_video_rental_store
 {
@@ -24,7 +23,7 @@ namespace asp.net_mvc_video_rental_store
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
