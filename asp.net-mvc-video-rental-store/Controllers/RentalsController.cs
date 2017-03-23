1283c9cf85dcdaa60c60ed8f8c68096634961ecf
Armin <armin.ag@outlook.com>
2017-03-23 19:37:18 +0100

    Add form for storing Rentals

---
+++ asp.net-mvc-video-rental-store/Controllers/RentalsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace asp.net_mvc_video_rental_store.Controllers
+{
+    public class RentalsController : Controller
+    {
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
