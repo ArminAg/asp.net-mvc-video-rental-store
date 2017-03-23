4664bef0d620cd735a2524d6b5e35c8aafb99b42
Armin <armin.ag@outlook.com>
2017-03-23 23:31:08 +0100

    Minor look improvements

--- asp.net-mvc-video-rental-store/App_Start/RouteConfig.cs
+++ asp.net-mvc-video-rental-store/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace asp.net_mvc_video_rental_store
@@ -16,7 +12,7 @@ namespace asp.net_mvc_video_rental_store
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Movies", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- asp.net-mvc-video-rental-store/Controllers/AccountController.cs
+++ asp.net-mvc-video-rental-store/Controllers/AccountController.cs
@@ -173,7 +173,7 @@ namespace asp.net_mvc_video_rental_store.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Movies");
                 }
                 AddErrors(result);
             }
@@ -408,7 +408,7 @@ namespace asp.net_mvc_video_rental_store.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Movies");
         }
 
         //
@@ -465,7 +465,7 @@ namespace asp.net_mvc_video_rental_store.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Movies");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
